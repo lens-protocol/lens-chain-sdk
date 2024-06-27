153a0220262f470fa929f301921414b5298f03d7
Cesare Naldi <naldi.cesare@gmail.com>
2024-06-27 16:45:48 +0200

    feat: ethers support for ERC-721 factory

--- src/ethers/typechain/index.ts
+++ src/ethers/typechain/index.ts
@@ -2,7 +2,11 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { BasicErc20 } from "./BasicErc20";
+export type { BasicErc721 } from "./BasicErc721";
 export type { Erc20Factory } from "./Erc20Factory";
+export type { Erc721Factory } from "./Erc721Factory";
 export * as factories from "./factories";
 export { BasicErc20__factory } from "./factories/BasicErc20__factory";
+export { BasicErc721__factory } from "./factories/BasicErc721__factory";
 export { Erc20Factory__factory } from "./factories/Erc20Factory__factory";
+export { Erc721Factory__factory } from "./factories/Erc721Factory__factory";
