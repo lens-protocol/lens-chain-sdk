939737192c3877cfb256b4513909dfef10f29f68
Cesare Naldi <naldi.cesare@gmail.com>
2024-12-02 18:09:24 +0100

    fix: updates chains setup

--- src/chains.ts
+++ src/chains.ts
@@ -1,6 +1,8 @@
 export type DeployedContracts = {
   // well known
   multicall3: `0x${string}`;
+  WETH: `0x${string}`;
+  WGRASS: `0x${string}`;
 
   // lens specific
   erc20Factory: `0x${string}`;
@@ -24,12 +26,14 @@ export const testnet: ChainDefinition = {
   id: 37111,
   name: 'Lens Testnet Network',
   network: 'lens-testnet-network',
-  rpcUrl: 'https://sepolia.rpc.lens.dev',
-  blockExplorerUrl: 'https://explorer.staging.lens.zksync.dev',
+  rpcUrl: 'https://rpc.testnet.lens.dev',
+  blockExplorerUrl: 'https://block-explorer.testnet.lens.dev/',
   testnet: true,
   contracts: {
     multicall3: '0x8A44EDE8a6843a997bC0Cc4659e4dB1Da8f91116',
     erc20Factory: '0x44D3f533C370C9Ed8cfbe2d77b4440DC74959508',
     erc721Factory: '0x1299E25078d91f3aE5ff8A44DEbc7f25b2A1f314',
+    WETH: '0xaA91D645D7a6C1aeaa5988e0547267B77d33fe16',
+    WGRASS: '0x1CfDf7a1F0C2A4eAe5f3C3eA8e3eBd4f8F2fEe4A',
   },
 };
