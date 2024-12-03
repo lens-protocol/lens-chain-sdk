47af10677fb2fe463cc1078151e93da2de9e4332
Cesare Naldi <naldi.cesare@gmail.com>
2024-12-03 10:49:44 +0100

    feat: viem example for EIP712 w/ hoisted local account bug

---
+++ examples/viem_bug_1/README.md
@@ -0,0 +1,3 @@
+# Viem Bug 1: Account Hoisting
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/lens-network/sdk/tree/main/examples/viem_bug_1)
---
+++ examples/viem_bug_1/index.ts
@@ -0,0 +1,33 @@
+import { chains } from '@lens-network/sdk/viem';
+import { parseAbi, Hex, createWalletClient, http, encodeFunctionData } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { eip712WalletActions, getGeneralPaymasterInput, sendEip712Transaction } from 'viem/zksync';
+
+const chain = chains.testnet;
+
+const account = privateKeyToAccount(process.env.PRIVATE_KEY as Hex);
+
+const walletClient = createWalletClient({
+  account,
+  chain,
+  transport: http(),
+}).extend(eip712WalletActions())
+
+const abi = parseAbi([
+  "function ping()"
+]);
+
+const hash = await sendEip712Transaction(walletClient, {
+  account: account.address,
+  to: '0x06a71429d153026a0F3bAdf481c216FDfe2d0629',
+  data: encodeFunctionData({
+    abi: abi,
+    functionName: 'ping'
+  }),
+  paymaster: '0xE6b37F099439ED1Eb38a97b909E9AD0ad74270Af',
+  paymasterInput: getGeneralPaymasterInput({
+    innerInput: '0x',
+  })
+})
+
+console.log(hash)
